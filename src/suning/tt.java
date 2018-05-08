1b10c70a5988d683a99251c2bfe2fb7c9a5fcd99
yankunpeng <yankunpeng_jiayou@163.com>
2018-05-08 19:42:36 +0800

    发的

---
+++ src/suning/tt.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C), 2002-2018, 苏宁易购电子商务有限公司
+ * FileName: tt.java
+ * Author:   18041949(闫昆朋)
+ * Date:     2018年5月8日 下午7:41:52
+ * Description: //描述当前类所属模块  
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package suning;
+/**
+ * @author 18041949(闫昆朋):
+ * @version 创建时间：2018年5月8日 下午7:41:52
+ * 类说明
+ */
+public class tt {
+   public static void main(String[] args) {
+	System.out.println("dd");
+}
+}
