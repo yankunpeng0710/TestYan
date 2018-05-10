1ab9e6f217ac89f5850dc8df71527225ec708622
yankunpeng <yankunpeng_jiayou@163.com>
2018-05-10 16:05:13 +0800

    33333

---
+++ src/suning/ge.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C), 2002-2018, 苏宁易购电子商务有限公司
+ * FileName: ge.java
+ * Author:   18041949(闫昆朋)
+ * Date:     2018年5月10日 下午4:04:40
+ * Description: //描述当前类所属模块  
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package suning;
+/**
+ * @author 18041949(闫昆朋):
+ * @version 创建时间：2018年5月10日 下午4:04:40
+ * 类说明
+ */
+public class ge {
+     public static void main(String[] args) {
+		System.out.println("gignaf");
+	}
+     
+}
